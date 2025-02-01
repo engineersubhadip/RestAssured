4828f9e6df83d467fa68f9f253898ca5b32087a7
Subhadip Das <subhadipdas911@gmail.com>
2025-02-01 12:42:04 +0530

    Captured the Dynamic Student ID, Verified the Fields for the ID, Deleted the same User via Dynamic ID

--- src/test/java/Session4/RequestPayloadViaHashMap.java
+++ src/test/java/Session4/RequestPayloadViaHashMap.java
@@ -1,16 +1,21 @@
 package Session4;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 import java.util.Arrays;
 import java.util.HashMap;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
 public class RequestPayloadViaHashMap {
 	
-	@Test
+	private Object id;
+	
+	@Test(priority=1)
 	public void testCase_CreateStudent() {
 		try {
 			HashMap<Object, Object> data = new HashMap<>();
@@ -20,13 +25,29 @@ public class RequestPayloadViaHashMap {
 			data.put("grade","N/A");
 			data.put("subjects",Arrays.asList("Computer Science", "Psycology"));
 			
-			given()
+			id = given()
 				.contentType("application/json")
 				.body(data)
 			.when()
 				.post("http://localhost:3000/students")
+				.jsonPath().getString("id");
+			
+			System.out.println("Test Case Execution Passed.");
+			Assert.assertTrue(true);
+			
+		} catch (Exception e) {
+			System.out.println("Test Case Execution Failed for testCase_CreateStudent. "+e.getMessage());
+			Assert.fail();
+		}
+	}
+	
+	@Test(priority=2, dependsOnMethods= {"testCase_CreateStudent"})
+	public void testCase_VerifyNewStudent() {
+		try {
+			when()
+				.get("http://localhost:3000/students/"+(String)id)
 			.then()
-				.statusCode(201)
+				.statusCode(200)
 				.body("name", equalTo("Subhadip Das"))
 				.body("age", equalTo(30))
 				.body("grade",equalTo("N/A"))
@@ -35,11 +56,29 @@ public class RequestPayloadViaHashMap {
 				.header("Content-Type","application/json")
 				.log().all();
 			
-			System.out.println("Test Case Execution Passed.");
+			System.out.println("Test Case execution passed for testCase_VerifyNewStudent.");
 			Assert.assertTrue(true);
 			
 		} catch (Exception e) {
-			System.out.println("Test Case Execution Failed for testCase_CreateStudent. "+e.getMessage());
+			System.out.println("Test Case execution failed for testCase_VerifyNewStudent. "+e.getMessage());
+			Assert.fail();
+		}
+	}
+	
+	
+
+	@Test(priority=3, dependsOnMethods= {"testCase_CreateStudent"})
+	public void testCase_DeleteStudent() {
+		try {
+			when()
+				.delete("http://localhost:3000/students/"+(String)id)
+			.then()
+				.log().all();
+			System.out.println("Test Case Exection Passed. testCase_DeleteStudent");
+			Assert.assertTrue(true);
+			
+		} catch (Exception e) {
+			System.out.println("Failed to execute test case testCase_DeleteStudent. "+e.getMessage());
 			Assert.fail();
 		}
 	}
