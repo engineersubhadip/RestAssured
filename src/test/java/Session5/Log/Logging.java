14f72340966856444626bc5326f841533bc998c0
Subhadip Das <subhadipdas911@gmail.com>
2025-02-02 12:41:01 +0530

    Print Response Cookies Only

--- src/test/java/Session5/Log/Logging.java
+++ src/test/java/Session5/Log/Logging.java
@@ -7,13 +7,27 @@ import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 public class Logging {
 	
-	@Test(priority=1) // log only response body
+//	@Test(priority=1) // log only response body
 	public void testCase_PrintResponseBody() {
 		try {
 			when()
 				.get("https://www.google.com/")
 			.then()
 				.log().body();
+			
+		} catch (Exception e ) {
+			System.out.println("Test case testCase_PrintResponseBody failed.");
+			Assert.fail();
+		}
+	}
+	
+	@Test(priority=2)
+	public void testCase_PrintCookies() {
+		try {
+			when()
+				.get("https://www.google.com/")
+			.then()
+				.log().cookies();
 			
 		} catch (Exception e ) {
 			System.out.println("Test case testCase_PrintResponseBody failed.");
