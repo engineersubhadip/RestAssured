64caaf303e49715536d2fd6f233ef3e08e2175eb
Subhadip Das <subhadipdas911@gmail.com>
2025-02-03 12:19:55 +0530

    Approach 2 using TestNG assertions

--- src/test/java/Session6/ParsingJSONResponseData.java
+++ src/test/java/Session6/ParsingJSONResponseData.java
@@ -1,6 +1,10 @@
 package Session6;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
@@ -17,4 +21,18 @@ public class ParsingJSONResponseData {
 			.header("Content-Type","application/json")
 			.body("book[4].title", equalTo("Harry Potter and the Sorcerer's Stone"));
 	}
+	
+	@Test(priority=2) // Approach-2 {More widely used in the real time environment, suitable for more complex validations, and more number of validations}
+	public void testCase_TestJSONResponse2() {
+		
+		Response res = when()
+			.get("http://localhost:3000/store");
+		
+		
+		Assert.assertEquals(200,res.getStatusCode());
+		Assert.assertEquals("application/json", res.getHeader("Content-Type"));
+		Assert.assertEquals("Harry Potter and the Sorcerer's Stone", res.jsonPath().get("book[4].title"));
+	}
+	
+	
 }
