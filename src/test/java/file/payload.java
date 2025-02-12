bfc405a27b2ff7047f7a1fbdbc2ff7e0e9494162
Subhadip Das <subhadipdas911@gmail.com>
2025-02-12 09:59:51 +0530

    Request Payload

---
+++ src/test/java/file/payload.java
@@ -0,0 +1,23 @@
+package file;
+
+public class payload {
+	
+	public static String addPlace() {
+		return "{\r\n"
+				+ "    \"location\": {\r\n"
+				+ "        \"lat\": -38.383494,\r\n"
+				+ "        \"lng\": 33.427362\r\n"
+				+ "    },\r\n"
+				+ "    \"accuracy\": 50,\r\n"
+				+ "    \"name\": \"Roadhouse Madness\",\r\n"
+				+ "    \"phone_number\": \"(+5) 983 893 3937\",\r\n"
+				+ "    \"address\": \"29, side layout, cohen 09\",\r\n"
+				+ "    \"types\": [\r\n"
+				+ "        \"shoe park\",\r\n"
+				+ "        \"shop\"\r\n"
+				+ "    ],\r\n"
+				+ "    \"website\": \"http://roadhousemadness.uk\",\r\n"
+				+ "    \"language\": \"French-IN\"\r\n"
+				+ "}";
+	}
+}
