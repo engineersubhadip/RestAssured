ed8ae47786d2be652f009a58c953ee4ea7a6895e
Subhadip Das <subhadipdas911@gmail.com>
2025-02-12 09:59:27 +0530

    Validate Status Code, Body Field (scope) and Header (Server)

--- src/test/java/Lecture1/AddPlaceAPI.java
+++ src/test/java/Lecture1/AddPlaceAPI.java
@@ -1,8 +1,10 @@
 package Lecture1;
 
-import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import file.payload;
+import src.main.resources.*;
 
 import io.restassured.RestAssured;
 
@@ -11,34 +13,22 @@ public class AddPlaceAPI {
 	public static void main(String[] args) {
 //		Base URI :-
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
-		
 		given()
 			.queryParam("key", "qaclick123")
 			.header("Content-Type", "application/json")
-			.body("{\r\n"
-					+ "  \"location\": {\r\n"
-					+ "    \"lat\": -38.383494,\r\n"
-					+ "    \"lng\": 33.427362\r\n"
-					+ "  },\r\n"
-					+ "  \"accuracy\": 50,\r\n"
-					+ "  \"name\": \"Roadhouse Madness\",\r\n"
-					+ "  \"phone_number\": \"(+5) 983 893 3937\",\r\n"
-					+ "  \"address\": \"29, side layout, cohen 09\",\r\n"
-					+ "  \"types\": [\r\n"
-					+ "    \"shoe park\",\r\n"
-					+ "    \"shop\"\r\n"
-					+ "  ],\r\n"
-					+ "  \"website\": \"http://roadhousemadness.uk\",\r\n"
-					+ "  \"language\": \"French-IN\"\r\n"
-					+ "}")
+			.body(payload.addPlace())
 			.log().all()
 		.when() // resource and http methods will always go under when method
 			.post("/maps/api/place/add/json")
 		.then()
-			.statusCode(205)
+			.statusCode(200)
 			.body("scope",equalTo("APP"))
 			.header("Server", "Apache/2.4.52 (Ubuntu)")
 			.log().all();
+		
+//		Add Place -> Update Place with new Address -> get Place to check if new address in shown or not
+		
+		
 	}
 
 }
