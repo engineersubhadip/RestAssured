b69f468e9c0e4c5b038b0a620bc5defcbadfacb8
Subhadip Das <subhadipdas911@gmail.com>
2025-02-13 10:57:20 +0530

    Updated addBook request mock data

--- src/test/java/file/payload.java
+++ src/test/java/file/payload.java
@@ -23,6 +23,6 @@ public class payload {
 
 	public static String addBook() {
 		return "{\r\n" + "\r\n" + "\"name\":\"Learn Appium Automation with Java\",\r\n" + "\"isbn\":\"bcd\",\r\n"
-				+ "\"aisle\":\"227\",\r\n" + "\"author\":\"John foe\"\r\n" + "}";
+				+ "\"aisle\":\"229\",\r\n" + "\"author\":\"John foe\"\r\n" + "}";
 	}
 }
